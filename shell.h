a7346a09ddcef4aca2fdbbc59ffc7fb5c7382578
lawrence-254 <lodhiambo26@gmai.com>
2023-05-14 05:13:38 -0700

    first two

---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <string.h>
+
+#define MAX_COMMAND 20
+
+void prompt(char **env, char *av);
+
+#endif
